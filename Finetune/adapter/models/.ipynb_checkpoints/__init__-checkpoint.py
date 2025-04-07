8182dfae43d0398bd0b6a1f80d5a0500fe77b5df
wowwowooo <143882364+wowwowooo@users.noreply.github.com>
2025-04-07 16:17:57 -0500

    Delete Finetune directory

--- Finetune/adapter/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from .CNN_S import CNN_S
-from .MLP_S import MLP_S
-from .LSTM_S import LSTM_S
-from .Tran_S import Tran_S
-# from .BiMamba_S import BiMamba_S
-
-__all__ = ["CNN_S", "MLP_S", "LSTM_S", "Tran_S"]
--- Finetune/adapter/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .CNN_S import CNN_S
-from .MLP_S import MLP_S
-from .LSTM_S import LSTM_S
-from .Tran_S import Tran_S
-
-__all__ = ["CNN_S", "MLP_S", "LSTM_S", "Tran_S"]
