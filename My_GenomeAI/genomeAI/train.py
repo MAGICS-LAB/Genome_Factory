c18ab802cefc9f2ed6541d259cc9b40e5a0d39bb
wowwowooo <143882364+wowwowooo@users.noreply.github.com>
2025-04-15 11:12:32 -0500

    Delete My_GenomeAI directory

--- My_GenomeAI/genomeAI/train.py
+++
@@ -1,4 +0,0 @@
-from workflow import train_sft
-
-if __name__ == "__main__":
-    train_sft()
--- My_GenomeAI/genomeAI/train_regression.py
+++
@@ -1,4 +0,0 @@
-from workflow_regression import train_sft
-
-if __name__ == "__main__":
-    train_sft()
