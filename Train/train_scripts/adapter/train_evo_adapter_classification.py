94b93d0b97e93c05ee524856321dca8e6e95de79
wowwowooo <143882364+wowwowooo@users.noreply.github.com>
2025-05-12 19:21:50 -0500

    Delete Train directory

--- Train/train_scripts/adapter/train_evo_adapter_classification.py
+++
@@ -1,4 +0,0 @@
-from genomeBridge.Train.workflow.adapter.evo_adapter_classification import train_sft
-
-if __name__ == "__main__":
-    train_sft()
--- Train/train_scripts/adapter/train_evo_adapter_regression.py
+++
@@ -1,4 +0,0 @@
-from genomeBridge.Train.workflow.adapter.evo_adapter_regression import train_sft
-
-if __name__ == "__main__":
-    train_sft()
